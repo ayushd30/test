b7dbcccc5d983460bcd6a521dede7338c1977a0d
ayushd30 <52347167+ayushd30@users.noreply.github.com>
2019-06-30 15:06:02 +0530

    Create question9.py

---
+++ question9.py
@@ -0,0 +1,2 @@
+print(tuple_var[:10]) :- this would print all the existing elements in the list ,as the "[:10]" is the condition printing all the elements till the 10th indexing position.
+print(tuple_var[10]):- This would provide us with an array.As there is no 10th position/index in the list.
