00868c43054ac218f4c3ae8e2e5f404ce633a5bc
ayushd30 <52347167+ayushd30@users.noreply.github.com>
2019-06-30 14:37:47 +0530

    Create Question2.py

---
+++ Question2.py
@@ -0,0 +1,14 @@
+import io
+list = [9,5]
+num = 6
+list[-1] = num
+print(list)
+ 
+#Question 3
+dict1={'name': 'ayush','naman''Age':[ 21 , 22] }
+dict2={'ID': ['ab1', 'ab2']}
+print(dict1)
+print(dict2)
+d1={}
+d1={**dict1,**dict2}
+print(d1)
